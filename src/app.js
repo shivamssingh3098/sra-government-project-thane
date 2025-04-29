804304dced44199edfef3bc45b1ccf30cee30a91
Shivam Singh <shivamssingh3098@gmail.com>
2025-04-29 20:52:44 +0530

    doon corse

--- src/app.js
+++ src/app.js
@@ -45,12 +45,14 @@ const allowedOrigins = [
 //   })
 // );
 
-app.use(
-  cors({
-    origin: "",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "",
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
