e9cf722722cb26b92aad01b0c51d2b6ff1c188bd
Shivam Singh <shivamssingh3098@gmail.com>
2025-05-13 18:13:53 +0530

    cludinary fial

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,17 +14,17 @@ console.log(
 // CLOUDINARY_API_KEY = 385278456424974;
 // CLOUDINARY_API_SECRET = KLLak1RknInIRL3_DBLoaZzDrRc;
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 // cloudinary.config({
-//   cloud_name: "dm6zltpzm",
-//   api_key: "385278456424974",
-//   api_secret: "KLLak1RknInIRL3_DBLoaZzDrRc",
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
 // });
+
+cloudinary.config({
+  cloud_name: "dm6zltpzm",
+  api_key: "385278456424974",
+  api_secret: "KLLak1RknInIRL3_DBLoaZzDrRc",
+});
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
